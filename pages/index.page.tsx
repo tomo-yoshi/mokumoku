f38009b14a2ebe6177bea8404e67eb09011eddea
Shiho(mia) <mia.can2021@gmail.com>
2022-09-25 15:40:44 -0700

    feat:install plugin-tailwindcss

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,16 +1,9 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello MokuMoku-Kai!!!
-        </h1>
-      </main>
-    </div>
-  )
-}
+    <h1 className="text-center font-mono underline">Hello MokuMoku-Kai!!!</h1>
+  );
+};
 
-export default Home
+export default Home;
