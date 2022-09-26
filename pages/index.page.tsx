c7695e53e8d2540e17476e644c7de323e145b909
Shiho(mia) <mia.can2021@gmail.com>
2022-09-25 17:28:10 -0700

    feat: install plugin-daisyui

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-center font-mono underline">Hello MokuMoku-Kai!!!</h1>
+    <>
+      <h1 className="text-center font-mono underline">Hello MokuMoku-Kai!!!</h1>
+      <button className="btn">Button</button>
+    </>
   );
 };
 
