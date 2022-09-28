7df26b9239c7c4d07c89a21fa9b4ab209a5466bc
Shiho Kazama <76821193+miacan2021@users.noreply.github.com>
2022-09-27 22:28:22 -0700

    Feature configure tailwindconfig (#21)
    * feat: configure main fonts and theme colors for tailwindCSS
    
    * feat: set theme colors of daisyui
    
    * Update tailwind.config.js

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -3,8 +3,10 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className="text-center font-mono underline">Hello MokuMoku-Kai!!!</h1>
-      <button className="btn">Button</button>
+      <h1 className="text-center underline font-primary bg-gradient-to-r from-theme_purple to-theme_blue">
+        Hello MokuMoku-Kai!!!
+      </h1>
+      <button className="btn btn-primary text-theme_blue">TEST</button>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./pages/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'primary':['Quicksand', 'sans-serif'],
+        'secondary': ['DM Serif Display', 'sans-serif']
+      },
+      colors: {
+        'theme_purple': '#E8C3FD',
+        'theme_blue': '#86C5FC',
+        'theme_black': '#231F20',
+        'theme_navy': '#345C85',
+      }
+    },
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+      mytheme: {    
+        "primary": "#E8C3FD",
+        "secondary": "#86C5FC",
+        "accent": "#345C85",
+        "neutral": "#FFFFFF",
+        "base-100": "#FFFFFF",
+        "info": "#86C5FC",
+        "success": "#E8C3FD",
+        "warning": "#E8C3FD",
+        "error": "#E8C3FD",
+      },
+      },
+    ],
+  },
+  plugins: [require("daisyui")]
 }
+
