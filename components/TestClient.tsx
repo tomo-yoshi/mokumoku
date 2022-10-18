9745b058bba64b6b664a198acfb9e12f46151f8f
Shiho(mia) <mia.can2021@gmail.com>
2022-10-17 20:30:09 -0700

    install apolloclient

---
+++ components/TestClient.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+type User = {
+  id: string | number;
+  name: string;
+};
+
+const getUserQuery = gql`
+  query {
+    getUser {
+      id
+      name
+    }
+  }
+`;
+
+const TestClient = () => {
+  const { data, error, loading } = useQuery(getUserQuery);
+  const users: [User] = data?.getUser;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!{error.message}</p>;
+  return (
+    <>
+      {users &&
+        users.map((user: User) => (
+          <div key={user.id}>
+            <h1>id:{user.id}</h1>
+            <h2>name:{user.name}</h2>
+          </div>
+        ))}
+    </>
+  );
+};
+export default TestClient;
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import TestClient from "../components/TestClient";
 
 const Home: NextPage = () => {
   return (
@@ -6,6 +7,7 @@ const Home: NextPage = () => {
       <h1 className="text-center underline font-primary bg-gradient-to-r from-theme_purple to-theme_blue">
         Hello MokuMoku-Kai!!!
       </h1>
+      <TestClient />
       <button className="btn btn-primary text-theme_blue">TEST</button>
     </>
   );
